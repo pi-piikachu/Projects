96f30c23df4d064d8d45376478cfcb5446e047ff
pi-piikachu <ahnikpurkait8846@gmail.com>
2024-04-17 16:52:47 +0530

    fetching api basic code written

--- recipe.js
+++ recipe.js
@@ -1,1 +1,33 @@
+const searchinp = document.querySelector('#searchinp');
+const searchbtn = document.querySelector('#src-btn');
+const recipecont = document.querySelector(".recipe-container");
 
+
+const url = `https://www.api.api-ninjas.com/v1/recipe?query=` ;
+const options = {
+    method : 'GET',
+    headers : { 'X-Api-Key': '20tSP4B16WRlKSpobsrJVA==PB3kaYZtWGA3sKUi'},
+};
+
+
+
+const fetchrecipe = async (query) => {
+    let response = await fetch(`https://api.api-ninjas.com/v1/recipe?query=${query}`,options);
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+    
+}
+searchbtn.addEventListener('click', (e) => {
+    e.preventDefault;
+    const recipeinp = searchinp.value.trim();
+    console.log("hello");
+   return fetchrecipe(recipeinp);
+})
+
+
+
+
+
+
+    
