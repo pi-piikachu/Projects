e3face7f86450c3b63cac6e9ed9ef251d0a0fea6
pi-piikachu <ahnikpurkait8846@gmail.com>
2024-04-18 03:40:35 +0530

    grid created in css, api fetched with img in js

--- recipe.js
+++ recipe.js
@@ -1,9 +1,40 @@
 const searchinp = document.querySelector('#searchinp');
+
+
+
+
 const searchbtn = document.querySelector('#src-btn');
-const recipecont = document.querySelector(".recipe-container");
+const recipecont = document.querySelector(`.recipecontainer`);
 
-
-const url = `https://www.api.api-ninjas.com/v1/recipe?query=` ;
+const fetchrecipe = async (query) => {
+    recipecont.innerHTML="";
+    let response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+  //  console.log(response);
+    let data = await response.json();
+    //console.log(data);
+    data.meals.forEach(element => {
+        let recipediv = document.createElement('div');
+        recipediv.classList.add('recipe');
+        console.log(element);
+         recipediv.innerHTML = `<img src = "${element.strMealThumb}">
+         <h3>${element.strMeal} </h3>\n
+                              
+         `;
+         
+         recipecont.appendChild(recipediv);
+        
+    
+    });
+    
+}
+searchbtn.addEventListener('click', (e) => {
+    e.preventDefault;
+    const recipeinp = searchinp.value.trim();
+    console.log("hello");
+   return fetchrecipe(recipeinp);
+})
+// api ninja api
+/*const url = `https://www.api.api-ninjas.com/v1/recipe?query=` ;
 const options = {
     method : 'GET',
     headers : { 'X-Api-Key': '20tSP4B16WRlKSpobsrJVA==PB3kaYZtWGA3sKUi'},
@@ -13,18 +44,22 @@ const options = {
 
 const fetchrecipe = async (query) => {
     let response = await fetch(`https://api.api-ninjas.com/v1/recipe?query=${query}`,options);
-    console.log(response);
+  //  console.log(response);
     let data = await response.json();
     console.log(data);
+    data.forEach(element => {
+        const recipediv = document.createElement('div');
+        recipediv.classList.add('recipe')
+         recipediv.innerHTML = `Name:${element.title} \n
+                                Ingredients:${element.ingredients}
+         `;
+         
+         recipecont.appendChild(recipediv);
+        
     
-}
-searchbtn.addEventListener('click', (e) => {
-    e.preventDefault;
-    const recipeinp = searchinp.value.trim();
-    console.log("hello");
-   return fetchrecipe(recipeinp);
-})
-
+    });
+    
+}*/
 
 
 
